2d7b8de054d9bcd964c1efe3d4caa490ce42cb25
Andy Lee <aglee@mac.com>
2020-09-20 20:24:23 -0400

    Initial Commit

---
+++ ScratchLabels/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ScratchLabels
+//
+//  Created by Andy Lee on 9/20/20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ScratchLabels/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  ScratchLabels
+//
+//  Created by Andy Lee on 9/20/20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // Setup code that might create autoreleased objects goes here.
+	}
+	return NSApplicationMain(argc, argv);
+}
