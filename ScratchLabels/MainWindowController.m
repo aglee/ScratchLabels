323b606df7467eeb1f6bdb1cccace63cb3ff2c6d
Andy Lee <aglee@mac.com>
2020-09-22 04:57:42 -0400

    Added a text view into which you paste addresses from the PDF file.

--- ScratchLabels/MainWindowController.m
+++ ScratchLabels/MainWindowController.m
@@ -10,6 +10,7 @@
 
 @interface MainWindowController ()
 
+@property (strong) IBOutlet NSTextView *addressesTextView;
 @property (strong) IBOutlet LabelPageView *pageView;
 
 @end
@@ -23,11 +24,7 @@
 	[printOperation runOperation];
 }
 
-#pragma mark - NSWindowController methods
-
-- (void)windowDidLoad {
-	[super windowDidLoad];
-
+- (IBAction)useFakeData:(id)sender {
 	NSMutableArray *fakeAddresses = [[NSMutableArray alloc] init];
 	for (NSInteger i = 0; i < 50; i++) {
 		MailingAddress *addr = [[MailingAddress alloc] init];
@@ -40,8 +37,68 @@
 
 		[fakeAddresses addObject:addr];
 	}
-
 	self.pageView.addresses = fakeAddresses;
+}
+
+#pragma mark - NSWindowController methods
+
+- (void)windowDidLoad {
+	[super windowDidLoad];
+
+	[self useFakeData:nil];
+}
+
+#pragma mark - <NSTextDelegate> methods
+
+- (void)textDidChange:(NSNotification *)notification {
+	NSArray<MailingAddress *> *addresses = [self _parseAddressesFromString:self.addressesTextView.textStorage.string];
+	self.pageView.addresses = addresses;
+}
+
+#pragma mark - Private methods
+
+static NSString *trim(NSString *s) {
+	return [s stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+}
+
+//TODO: Add error handling.
+- (NSArray<MailingAddress *> *)_parseAddressesFromString:(NSString *)s {
+	NSMutableArray<MailingAddress *> *addresses = [[NSMutableArray alloc] init];
+	NSArray<NSString *> *lines = [self _scrubLines:[s componentsSeparatedByString:@"\n"]];
+	NSInteger lineIndex = 0;
+	while (lines.count - lineIndex >= 7) {
+		NSArray<NSString *> *addressFields = [lines subarrayWithRange:NSMakeRange(lineIndex, 7)];
+		lineIndex += 7;
+		[addresses addObject:[self _makeAddressFromFields:addressFields]];
+	}
+	return addresses;
+}
+
+- (NSArray<NSString *> *)_scrubLines:(NSArray<NSString *> *)rawLines {
+	NSMutableArray<NSString *> *scrubbedLines = [[NSMutableArray alloc] init];
+
+	for (NSString *line in rawLines) {
+		NSString *trimmedLine = trim(line);
+		if (trimmedLine.length > 0) {
+			[scrubbedLines addObject:trimmedLine];
+		}
+	}
+
+	return scrubbedLines;
+}
+
+- (MailingAddress *)_makeAddressFromFields:(NSArray<NSString *> *)addressFields {
+	MailingAddress *addr = [[MailingAddress alloc] init];
+
+	// First field is a row ID of some kind, we ignore it.
+	addr.name = addressFields[1];
+	addr.street = addressFields[2];
+	addr.city = addressFields[3];
+	addr.state = addressFields[4];
+	addr.zip = addressFields[5];
+	addr.zipAddOn = addressFields[6];
+	
+	return addr;
 }
 
 @end
