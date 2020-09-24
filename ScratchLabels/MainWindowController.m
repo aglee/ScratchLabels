cf9ba05bbedf32675a3fc8abd93017af7128bb7c
Andy Lee <aglee@mac.com>
2020-09-24 13:53:59 -0400

    Handle data with one address per line (vs. one field per line).

--- ScratchLabels/MainWindowController.m
+++ ScratchLabels/MainWindowController.m
@@ -10,8 +10,17 @@
 #import "SLBPrintPanel.h"
 
 @interface MainWindowController ()
+
+/// How to parse the text in addressesTextView.  The first time I got GPP data it was in a PDF file, and when I copy-pasted
+/// the text the result had one *field* per line.  The second time, I got an Excel file with one *address* per line, with
+/// tab-separated fields.  I made the first request by email.  The second time, I used the new Google form to request the
+/// addresses.  I expect to be using the Google form henceforth, and I'm guessing I'll keep getting Excel files, so this
+/// defaults to YES.
+@property (assign) BOOL oneAddressPerLine;
+
 @property (strong) IBOutlet NSTextView *addressesTextView;
 @property (strong) IBOutlet LabelPageView *pageView;
+
 @end
 
 @implementation MainWindowController
@@ -46,6 +55,8 @@
 - (void)windowDidLoad {
 	[super windowDidLoad];
 
+	self.oneAddressPerLine = YES;
+
 	[self useFakeData:nil];
 }
 
@@ -58,34 +69,57 @@
 
 #pragma mark - Private methods
 
-static NSString *trim(NSString *s) {
-	return [s stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-}
-
 //TODO: Add error handling.
 - (NSArray<MailingAddress *> *)_parseAddressesFromString:(NSString *)s {
 	NSMutableArray<MailingAddress *> *addresses = [[NSMutableArray alloc] init];
-	NSArray<NSString *> *lines = [self _scrubLines:[s componentsSeparatedByString:@"\n"]];
-	NSInteger lineIndex = 0;
-	while (lines.count - lineIndex >= 7) {
-		NSArray<NSString *> *addressFields = [lines subarrayWithRange:NSMakeRange(lineIndex, 7)];
-		lineIndex += 7;
+	NSArray<NSString *> *lines = [self _removeBlanksFromLines:[s componentsSeparatedByString:@"\n"]];
+	NSArray<NSArray<NSString *> *> *groupsOfFields = [self _parseGroupsOfFieldsFromLines:lines];
+	for (NSArray<NSString *> *addressFields in groupsOfFields) {
 		[addresses addObject:[self _makeAddressFromFields:addressFields]];
 	}
 	return addresses;
 }
 
-- (NSArray<NSString *> *)_scrubLines:(NSArray<NSString *> *)rawLines {
+/// Trims whitespace from each line, and removes empty lines.
+- (NSArray<NSString *> *)_removeBlanksFromLines:(NSArray<NSString *> *)rawLines {
 	NSMutableArray<NSString *> *scrubbedLines = [[NSMutableArray alloc] init];
 
 	for (NSString *line in rawLines) {
-		NSString *trimmedLine = trim(line);
+		NSString *trimmedLine = [line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
 		if (trimmedLine.length > 0) {
 			[scrubbedLines addObject:trimmedLine];
 		}
 	}
 
 	return scrubbedLines;
+}
+
+/// Assumes _removeBlanksFromLines: was called, so the given array of lines contains no extraneous whitespace.
+- (NSArray<NSArray<NSString *> *> *)_parseGroupsOfFieldsFromLines:(NSArray<NSString *> *)lines {
+	NSMutableArray<NSArray<NSString *> *> *groupsOfFields = [[NSMutableArray alloc] init];
+
+	if (self.oneAddressPerLine) {
+		// Expect each line to contain all the fields of an address, separated by tabs.
+		// Skip any line that doesn't start with a VAN ID -- assume it contains Excel
+		// column titles.  ("VAN" = "voter activation network".)
+		for (NSString *line in lines) {
+			NSArray<NSString *> *group = [line componentsSeparatedByString:@"\t"];
+			NSString *firstField = group.firstObject;
+			if (firstField.length < 7 || ![firstField isEqualToString:@(firstField.integerValue).stringValue]) {
+				continue;
+			}
+			[groupsOfFields addObject:group];
+		}
+	} else {
+		// Make the first 7 lines into a group, then the next 7 lines, then the next 7 lines, etc.
+		NSInteger lineIndex = 0;
+		while (lines.count - lineIndex >= 7) {
+			[groupsOfFields addObject:[lines subarrayWithRange:NSMakeRange(lineIndex, 7)]];
+			lineIndex += 7;
+		}
+	}
+
+	return groupsOfFields;
 }
 
 - (MailingAddress *)_makeAddressFromFields:(NSArray<NSString *> *)addressFields {
